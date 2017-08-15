261c5a1ef6afb59ffee1a54c55ab052d60565eb0
Petrick <petrickandrew@gmail.com>
2017-08-15 19:48:58 +0300

    Jenkins start JUnitTest

--- src/main/java/JustTest.java
+++ src/main/java/JustTest.java
@@ -2,26 +2,24 @@
  * Run simple selenium tests in Chrome browser
  */
 
-    import org.junit.*;
-    import org.openqa.selenium.By;
-    import org.openqa.selenium.WebDriver;
-    import org.openqa.selenium.WebElement;
-    import org.openqa.selenium.chrome.ChromeDriver;
-    import org.openqa.selenium.chrome.ChromeOptions;
-    import org.openqa.selenium.remote.DesiredCapabilities;
-    import java.util.HashMap;
-    import java.util.Map;
+    import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
     
     //java -jar /usr/share/jenkins/jenkins.war
 
 public class JustTest {
-    String chromePath = "/drivers/chromedriver";
+  //  String chromePath = "C:/GitRepoJUnit/simple-junit-test/drivers/chromedriver.exe";
     WebDriver driver;
 
     @Before
     public void startChrome() {
-        setChromeEnvVariable();
-        driver = new ChromeDriver(getChromeCapabilities());
+   //     setChromeEnvVariable();
+        driver = new ChromeDriver();
     }
 
     @Test
@@ -48,30 +46,30 @@ public class JustTest {
         searchBox.submit();
     }
 
-    private void setChromeEnvVariable() {
-        System.setProperty("webdriver.chrome.driver", getChromePath());
-    }
-
-    private String getChromePath(){
-        return System.getProperty("user.dir") + chromePath;
-    }
-
-    private DesiredCapabilities getChromeCapabilities() {
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, getChromeOptions());
-        return capabilities;
-    }
-
-    private ChromeOptions getChromeOptions() {
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("mobileEmulation", getMobileEmulation());
-        return options;
-    }
-
-    private Map<String, String> getMobileEmulation() {
-        Map<String, String> mobileEmulation = new HashMap<String, String>();
-        mobileEmulation.put("deviceName", "Google Nexus 10");
-        return mobileEmulation;
-    }
+//    private void setChromeEnvVariable() {
+//        System.setProperty("webdriver.chrome.driver", getChromePath());
+//    }
+//
+//    private String getChromePath(){
+//        return System.getProperty("user.dir") + chromePath;
+//    }
+//
+//    private DesiredCapabilities getChromeCapabilities() {
+//        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//        capabilities.setCapability(ChromeOptions.CAPABILITY, getChromeOptions());
+//        return capabilities;
+//    }
+//
+//    private ChromeOptions getChromeOptions() {
+//        ChromeOptions options = new ChromeOptions();
+//        options.setExperimentalOption("mobileEmulation", getMobileEmulation());
+//        return options;
+//    }
+//
+//    private Map<String, String> getMobileEmulation() {
+//        Map<String, String> mobileEmulation = new HashMap<String, String>();
+//        mobileEmulation.put("deviceName", "Google Nexus 10");
+//        return mobileEmulation;
+//    }
 
 }
